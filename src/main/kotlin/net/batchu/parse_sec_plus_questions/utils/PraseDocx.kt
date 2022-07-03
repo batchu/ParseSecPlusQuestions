bf498f095aaaccc1890643f3bca85c3a5762cf75
Prashanth Batchu <prashanth.batchu@imail.org>
2022-07-02 23:35:47 -0500

    Additional fixes in case the question is multi line

--- src/main/kotlin/net/batchu/parse_sec_plus_questions/utils/PraseDocx.kt
+++ src/main/kotlin/net/batchu/parse_sec_plus_questions/utils/PraseDocx.kt
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Component
 import java.io.File
+import java.util.stream.Collectors
 
 @Component
 class ParseDocx: CommandLineRunner{
@@ -28,6 +29,7 @@ class ParseDocx: CommandLineRunner{
                 val wordExtractor = XWPFWordExtractor(targetDoc)
                 val parseQuestions = parseQuestions(wordExtractor.text)
                 repo.saveAll(parseQuestions)
+                LOG.info("Saved ${parseQuestions.size} entries to DB")
                 inputStream.close()
             }
         }
@@ -43,17 +45,17 @@ class ParseDocx: CommandLineRunner{
                 var no = ""
                 list.mapIndexed { index, it ->
                     LOG.debug("Processing index ${index}")
-                    val inQuestion = (index > 0 && list[index - 1].startsWith("QUESTION "))
+                    var inQuestion = (index > 0 && ((list[index - 1]).startsWith("QUESTION ") || (list[index - 2]).startsWith("QUESTION ") || (list[index - 3]).startsWith("QUESTION ")))
                     when {
                         it.startsWith("QUESTION ") -> Triple(it.substringAfter("QUESTION ").trim().apply { no = this }, "id", null)
-                        inQuestion                 -> Triple(no, "Question" , it.trim())
-                        it.trimStart().startsWith("A. ")       -> Triple(no, "Answer A" , it.substringAfter(". ").trim())
+                        it.trimStart().startsWith("A. ")       -> Triple(no, "Answer A" , it.substringAfter(". ").trim().apply { inQuestion = false })
                         it.trimStart().startsWith("B. ")       -> Triple(no, "Answer B" , it.substringAfter(". ").trim())
                         it.trimStart().startsWith("C. ")       -> Triple(no, "Answer C" , it.substringAfter(". ").trim())
                         it.trimStart().startsWith("D. ")       -> Triple(no, "Answer D" , it.substringAfter(". ").trim())
                         it.trimStart().startsWith("E. ")       -> Triple(no, "Answer E" , it.substringAfter(". ").trim())
                         it.trimStart().startsWith("F. ")       -> Triple(no, "Answer F" , it.substringAfter(". ").trim())
                         it.trimStart().startsWith("G. ")       -> Triple(no, "Answer G" , it.substringAfter(". ").trim())
+                        inQuestion                 -> Triple(no, "Question" , it.trim())
                         it.trimStart().startsWith("Answer: ") ||  it.contains("Answer:")  -> Triple(no, "Answer" , it.substringAfter(": ").trim())
                         else                       -> Triple(no, "Comment" , it)
                     }
@@ -66,7 +68,7 @@ class ParseDocx: CommandLineRunner{
                 Question(
                     id = id,
                     question = list
-                        .first { it.second == "Question" }.third!!,
+                        .filter { it.second == "Question" }.map{it.third}.stream().collect(Collectors.joining(" ")),
                     choices = list
                         .filter { it.second.startsWith("Answer ") }
                         .map { Choice(choice = it.third!!, isCorrect = it.second.last() == correctAnswer) },
